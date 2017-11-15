4dc8e3497be965b3ed30c996f97b3661f8b74ab6
Edi Rose <edirose1998@gmail.com>
2017-11-15 13:52:52 +1300

    added sprint-5 repo and eloquent.js

---
+++ sprint-5/eloquesnt-2.js
@@ -0,0 +1,18 @@
+for (var hashtags = "#"; hastags.length < 8; hashtags += "#")
+  console.log(hashtags);
+
+
+var number = 1;
+for (number <= 100; number++) {
+  if (number % 3) {
+    console.log("Fizz");
+  }
+  else if (number % 5) {
+    console.log("Buzz");
+  }
+  else {
+    console.log(number);
+  }
+}
+
+//couldnt crack the third one//ß
